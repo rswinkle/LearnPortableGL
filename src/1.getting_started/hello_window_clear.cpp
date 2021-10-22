88db0ee00ba5cf7021f20e7753cbb59540dc7164
Robert Winkler <rob121618@gmail.com>
2021-10-22 11:35:58 -0700

    Add hello_window_clear

---
+++ src/1.getting_started/hello_window_clear.cpp
@@ -0,0 +1,125 @@
+#define MANGLE_TYPES
+#define PORTABLEGL_IMPLEMENTATION
+#include <portablegl.h>
+
+#include <iostream>
+
+#define SDL_MAIN_HANDLED
+#include <SDL2/SDL.h>
+
+bool handle_events();
+
+// settings
+unsigned int scr_width = 640;
+unsigned int scr_height = 480;
+
+#define PIX_FORMAT SDL_PIXELFORMAT_ARGB8888
+
+SDL_Window* window;
+SDL_Renderer* ren;
+SDL_Texture* tex;
+u32* bbufpix;
+
+glContext the_Context;
+
+int main()
+{
+	// Initialize SDL2 and create window
+	// ------------------------------
+	SDL_SetMainReady();
+	if (SDL_Init(SDL_INIT_VIDEO)) {
+		std::cout << "SDL_Init error: " << SDL_GetError() << "\n";
+		return 0;
+	}
+
+	window = SDL_CreateWindow("camera_class", 100, 100, scr_width, scr_height, SDL_WINDOW_SHOWN | SDL_WINDOW_RESIZABLE);
+	if (!window) {
+		std::cerr << "Failed to create window\n";
+		SDL_Quit();
+		return 0;
+	}
+
+	// Create software renderer and texture
+	ren = SDL_CreateRenderer(window, -1, SDL_RENDERER_SOFTWARE);
+	tex = SDL_CreateTexture(ren, PIX_FORMAT, SDL_TEXTUREACCESS_STREAMING, scr_width, scr_height);
+
+	// Initialize and set PGL context
+	if (!init_glContext(&the_Context, &bbufpix, scr_width, scr_height, 32, 0x00FF0000, 0x0000FF00, 0x000000FF, 0xFF000000)) {
+		puts("Failed to initialize glContext");
+		exit(0);
+	}
+	set_glContext(&the_Context);
+
+	// render loop
+	// -----------
+	while (true)
+	{
+		// input
+		// -----
+		if (handle_events())
+			break;
+
+		// render
+		// ------
+		glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+		glClear(GL_COLOR_BUFFER_BIT);
+
+		// SDL2: Update SDL_Texture to latest rendered frame, then blit to screen
+		// ----------------------------------------------------------------------
+		SDL_UpdateTexture(tex, NULL, bbufpix, scr_width * sizeof(u32));
+		SDL_RenderCopy(ren, tex, NULL, NULL);
+		SDL_RenderPresent(ren);
+	}
+
+	// SDL and PortableGL cleanup
+	// ------------------------------------------------------------------
+	free_glContext(&the_Context);
+
+	SDL_DestroyTexture(tex);
+	SDL_DestroyRenderer(ren);
+	SDL_DestroyWindow(window);
+
+	SDL_Quit();
+	return 0;
+}
+
+// process all input: Poll for events reacting to ones we care about
+// ---------------------------------------------------------------------------------------------------------
+bool handle_events()
+{
+	SDL_Event event;
+	SDL_Scancode sc;
+
+	while (SDL_PollEvent(&event)) {
+		switch (event.type) {
+		case SDL_QUIT:
+			return true;
+		case SDL_KEYDOWN:
+			sc = event.key.keysym.scancode;
+			
+			if (sc == SDL_SCANCODE_ESCAPE) {
+				return true;
+			}
+			break;
+
+		case SDL_WINDOWEVENT:
+			switch (event.window.event) {
+			case SDL_WINDOWEVENT_RESIZED:
+				scr_width = event.window.data1;
+				scr_height = event.window.data2;
+
+				bbufpix = (u32*)pglResizeFramebuffer(scr_width, scr_height);
+
+				glViewport(0, 0, scr_width, scr_height);
+				SDL_DestroyTexture(tex);
+				tex = SDL_CreateTexture(ren, PIX_FORMAT, SDL_TEXTUREACCESS_STREAMING, scr_width, scr_height);
+				break;
+			}
+			break;
+		}
+	}
+
+
+	return false;
+}
+
--- src/1.getting_started/premake5.lua
+++ src/1.getting_started/premake5.lua
@@ -3,6 +3,9 @@ project "1.1.1.hello_window"
 	kind "ConsoleApp"
 	files "hello_window.cpp"
 
+project "1.1.2.hello_window_clear"
+	kind "ConsoleApp"
+	files "hello_window_clear.cpp"
 
 project "1.7.4.camera_class"
 	kind "ConsoleApp"
