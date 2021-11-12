564dd41a49c59311f7dedb2b1bcad37352c2e381
Robert Winkler <rob121618@gmail.com>
2021-11-11 17:06:35 -0700

    Add basic lighting exercises

---
+++ src/2.lighting/basic_lighting_exercise3.cpp
@@ -0,0 +1,436 @@
+#define MANGLE_TYPES
+#define PORTABLEGL_IMPLEMENTATION
+#include <portablegl.h>
+
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
+
+#include <learnopengl/filesystem.h>
+#include <learnopengl/camera.h>
+
+#include <iostream>
+
+#define SDL_MAIN_HANDLED
+#include <SDL2/SDL.h>
+
+using namespace glm;
+
+struct My_Uniforms
+{
+	mat4 model;
+	mat4 view;
+	mat4 projection;
+
+	vec3 objectColor;
+	vec3 lightColor;
+	vec3 lightPos;
+	vec3 viewPos;
+};
+
+void setup_context();
+void cleanup();
+bool handle_events();
+
+void basic_lighting_vs(float* vs_output, void* vertex_attribs, Shader_Builtins* builtins, void* uniforms);
+void basic_lighting_fs(float* fs_input, Shader_Builtins* builtins, void* uniforms);
+
+void light_cube_vs(float* vs_output, void* vertex_attribs, Shader_Builtins* builtins, void* uniforms);
+void light_cube_fs(float* fs_input, Shader_Builtins* builtins, void* uniforms);
+
+// settings
+unsigned int scr_width = 640;
+unsigned int scr_height = 480;
+
+// camera
+Camera camera(vec3(0.0f, 0.0f, 3.0f));
+
+// timing
+float deltaTime = 0.0f;
+float lastFrame = 0.0f;
+
+// lighting
+vec3 lightPos(1.2f, 1.0f, 2.0f);
+
+SDL_Window* window;
+SDL_Renderer* ren;
+SDL_Texture* tex;
+
+u32* bbufpix;
+
+glContext the_Context;
+
+My_Uniforms uniforms;
+
+int main()
+{
+	// SDL2 and PortableGL: initialize and configure
+	// ------------------------------
+	setup_context();
+
+	// tell SDL to capture our mouse
+	SDL_SetRelativeMouseMode(SDL_TRUE);
+
+	// configure global opengl state
+	// -----------------------------
+	glEnable(GL_DEPTH_TEST);
+
+	// Create our shader programs and set uniform pointer for each
+	// -----------------------------------------------------------
+	GLenum smooth[] = { SMOOTH, SMOOTH, SMOOTH };
+	GLuint lightingShader = pglCreateProgram(basic_lighting_vs, basic_lighting_fs, 3, smooth, GL_FALSE);
+	glUseProgram(lightingShader);
+	pglSetUniform(&uniforms);
+
+	GLuint lightCubeShader = pglCreateProgram(light_cube_vs, light_cube_fs, 0, NULL, GL_FALSE);
+	glUseProgram(lightCubeShader);
+	pglSetUniform(&uniforms);
+
+	// set up vertex data (and buffer(s)) and configure vertex attributes
+	// ------------------------------------------------------------------
+	float vertices[] = {
+		-0.5f, -0.5f, -0.5f,  0.0f,  0.0f, -1.0f,
+		 0.5f, -0.5f, -0.5f,  0.0f,  0.0f, -1.0f,
+		 0.5f,  0.5f, -0.5f,  0.0f,  0.0f, -1.0f,
+		 0.5f,  0.5f, -0.5f,  0.0f,  0.0f, -1.0f,
+		-0.5f,  0.5f, -0.5f,  0.0f,  0.0f, -1.0f,
+		-0.5f, -0.5f, -0.5f,  0.0f,  0.0f, -1.0f,
+
+		-0.5f, -0.5f,  0.5f,  0.0f,  0.0f,  1.0f,
+		 0.5f, -0.5f,  0.5f,  0.0f,  0.0f,  1.0f,
+		 0.5f,  0.5f,  0.5f,  0.0f,  0.0f,  1.0f,
+		 0.5f,  0.5f,  0.5f,  0.0f,  0.0f,  1.0f,
+		-0.5f,  0.5f,  0.5f,  0.0f,  0.0f,  1.0f,
+		-0.5f, -0.5f,  0.5f,  0.0f,  0.0f,  1.0f,
+
+		-0.5f,  0.5f,  0.5f, -1.0f,  0.0f,  0.0f,
+		-0.5f,  0.5f, -0.5f, -1.0f,  0.0f,  0.0f,
+		-0.5f, -0.5f, -0.5f, -1.0f,  0.0f,  0.0f,
+		-0.5f, -0.5f, -0.5f, -1.0f,  0.0f,  0.0f,
+		-0.5f, -0.5f,  0.5f, -1.0f,  0.0f,  0.0f,
+		-0.5f,  0.5f,  0.5f, -1.0f,  0.0f,  0.0f,
+
+		 0.5f,  0.5f,  0.5f,  1.0f,  0.0f,  0.0f,
+		 0.5f,  0.5f, -0.5f,  1.0f,  0.0f,  0.0f,
+		 0.5f, -0.5f, -0.5f,  1.0f,  0.0f,  0.0f,
+		 0.5f, -0.5f, -0.5f,  1.0f,  0.0f,  0.0f,
+		 0.5f, -0.5f,  0.5f,  1.0f,  0.0f,  0.0f,
+		 0.5f,  0.5f,  0.5f,  1.0f,  0.0f,  0.0f,
+
+		-0.5f, -0.5f, -0.5f,  0.0f, -1.0f,  0.0f,
+		 0.5f, -0.5f, -0.5f,  0.0f, -1.0f,  0.0f,
+		 0.5f, -0.5f,  0.5f,  0.0f, -1.0f,  0.0f,
+		 0.5f, -0.5f,  0.5f,  0.0f, -1.0f,  0.0f,
+		-0.5f, -0.5f,  0.5f,  0.0f, -1.0f,  0.0f,
+		-0.5f, -0.5f, -0.5f,  0.0f, -1.0f,  0.0f,
+
+		-0.5f,  0.5f, -0.5f,  0.0f,  1.0f,  0.0f,
+		 0.5f,  0.5f, -0.5f,  0.0f,  1.0f,  0.0f,
+		 0.5f,  0.5f,  0.5f,  0.0f,  1.0f,  0.0f,
+		 0.5f,  0.5f,  0.5f,  0.0f,  1.0f,  0.0f,
+		-0.5f,  0.5f,  0.5f,  0.0f,  1.0f,  0.0f,
+		-0.5f,  0.5f, -0.5f,  0.0f,  1.0f,  0.0f
+	};
+	// first, configure the cube's VAO (and VBO)
+	unsigned int VBO, cubeVAO;
+	glGenVertexArrays(1, &cubeVAO);
+	glGenBuffers(1, &VBO);
+
+	glBindBuffer(GL_ARRAY_BUFFER, VBO);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+	glBindVertexArray(cubeVAO);
+
+	// position attribute
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), 0);
+	glEnableVertexAttribArray(0);
+	// normal attribute
+	glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), 3 * sizeof(float));
+	glEnableVertexAttribArray(1);
+
+
+	// second, configure the light's VAO (VBO stays the same; the vertices are the same for the light object which is also a 3D cube)
+	unsigned int lightCubeVAO;
+	glGenVertexArrays(1, &lightCubeVAO);
+	glBindVertexArray(lightCubeVAO);
+
+	glBindBuffer(GL_ARRAY_BUFFER, VBO);
+	// note that we update the lamp's position attribute's stride to reflect the updated buffer data
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), 0);
+	glEnableVertexAttribArray(0);
+	// render loop
+	// -----------
+	while (true)
+	{
+		// per-frame time logic
+		// --------------------
+		int currentFrame = SDL_GetTicks();
+		deltaTime = (currentFrame - lastFrame)/1000.0f;
+		lastFrame = currentFrame;
+
+		// input
+		// -----
+		if (handle_events())
+			break;
+
+		// render
+		// ------
+		glClearColor(0.1f, 0.1f, 0.1f, 1.0f);
+		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+		
+		// be sure to activate shader when setting uniforms/drawing objects
+		glUseProgram(lightingShader);
+		uniforms.objectColor = vec3(1.0f, 0.5f, 0.31f);
+		uniforms.lightColor = vec3(1.0f, 1.0f, 1.0f);
+		uniforms.lightPos = lightPos;
+		uniforms.viewPos = camera.Position;
+
+		// view/projection transformations
+		uniforms.projection = perspective(radians(camera.Zoom), (float)scr_width / (float)scr_height, 0.1f, 100.0f);
+		uniforms.view = camera.GetViewMatrix();
+
+		// world transformation
+		uniforms.model = mat4(1.0f);
+
+		// render the cube
+		glBindVertexArray(cubeVAO);
+		glDrawArrays(GL_TRIANGLES, 0, 36);
+
+		// also draw the lamp object
+		glUseProgram(lightCubeShader);
+		// projection and view matrices are already set correctly
+		mat4 model = mat4(1.0f);
+		model = translate(model, lightPos);
+		uniforms.model = scale(model, vec3(0.2f)); // a smaller cube
+
+		glBindVertexArray(lightCubeVAO);
+		glDrawArrays(GL_TRIANGLES, 0, 36);
+
+		// SDL2: blit texture (ie latest rendered frame) to screen
+		// -------------------------------------------------------------------------------
+		SDL_UpdateTexture(tex, NULL, bbufpix, scr_width * sizeof(u32));
+		SDL_RenderCopy(ren, tex, NULL, NULL);
+		SDL_RenderPresent(ren);
+	}
+
+	// optional: de-allocate all resources once they've outlived their purpose:
+	// ------------------------------------------------------------------------
+	glDeleteVertexArrays(1, &cubeVAO);
+	glDeleteVertexArrays(1, &lightCubeVAO);
+	glDeleteBuffers(1, &VBO);
+
+	// SDL and PortableGL cleanup
+	// ------------------------------------------------------------------
+	cleanup();
+	return 0;
+}
+
+// process all input: Poll for events reacting to ones we care about
+// ---------------------------------------------------------------------------------------------------------
+bool handle_events()
+{
+	SDL_Event event;
+	SDL_Scancode sc;
+
+	while (SDL_PollEvent(&event)) {
+		switch (event.type) {
+		case SDL_QUIT:
+			return true;
+		case SDL_KEYDOWN:
+			sc = event.key.keysym.scancode;
+			
+			switch (sc) {
+			case SDL_SCANCODE_ESCAPE:
+				return true;
+
+			/*
+			case SDL_SCANCODE_W:
+				camera.ProcessKeyboard(FORWARD, deltaTime);
+				break;
+			case SDL_SCANCODE_S:
+				camera.ProcessKeyboard(BACKWARD, deltaTime);
+				break;
+			case SDL_SCANCODE_A:
+				camera.ProcessKeyboard(LEFT, deltaTime);
+				break;
+			case SDL_SCANCODE_D:
+				camera.ProcessKeyboard(RIGHT, deltaTime);
+				break;
+				*/
+			default:
+				;
+			}
+
+			break; //sdl_keydown
+
+		case SDL_WINDOWEVENT:
+			switch (event.window.event) {
+			case SDL_WINDOWEVENT_RESIZED:
+				scr_width = event.window.data1;
+				scr_height = event.window.data2;
+
+				bbufpix = (u32*)pglResizeFramebuffer(scr_width, scr_height);
+				glViewport(0, 0, scr_width, scr_height);
+				SDL_DestroyTexture(tex);
+				tex = SDL_CreateTexture(ren, SDL_PIXELFORMAT_ARGB8888, SDL_TEXTUREACCESS_STREAMING, scr_width, scr_height);
+				break;
+			}
+			break;
+
+		case SDL_MOUSEMOTION:
+		{
+			float dx = event.motion.xrel;
+			float dy = -event.motion.yrel; // reversed since y coordinates go from bottom to top
+			camera.ProcessMouseMovement(dx, dy);
+		} break;
+
+		case SDL_MOUSEWHEEL:
+			camera.ProcessMouseScroll(event.wheel.y);
+			break;
+		}
+	}
+
+	const Uint8 *state = SDL_GetKeyboardState(NULL);
+	
+	if (state[SDL_SCANCODE_W]) {
+		camera.ProcessKeyboard(FORWARD, deltaTime);
+	}
+	if (state[SDL_SCANCODE_S]) {
+		camera.ProcessKeyboard(BACKWARD, deltaTime);
+	}
+	if (state[SDL_SCANCODE_A]) {
+		camera.ProcessKeyboard(LEFT, deltaTime);
+	}
+	if (state[SDL_SCANCODE_D]) {
+		camera.ProcessKeyboard(RIGHT, deltaTime);
+	}
+
+	return false;
+}
+
+/*
+// glfw: whenever the window size changed (by OS or user resize) this callback function executes
+// ---------------------------------------------------------------------------------------------
+void framebuffer_size_callback(GLFWwindow* window, int width, int height)
+{
+	// make sure the viewport matches the new window dimensions; note that width and
+	// height will be significantly larger than specified on retina displays.
+	glViewport(0, 0, width, height);
+}
+*/
+
+
+
+void setup_context()
+{
+	// Initialize SDL2 and create window
+	SDL_SetMainReady();
+	if (SDL_Init(SDL_INIT_VIDEO)) {
+		std::cout << "SDL_Init error: " << SDL_GetError() << "\n";
+		exit(0);
+	}
+
+	window = SDL_CreateWindow("LearnPortablGL", 100, 100, scr_width, scr_height, SDL_WINDOW_SHOWN | SDL_WINDOW_RESIZABLE);
+	if (!window) {
+		std::cerr << "Failed to create window\n";
+		SDL_Quit();
+		exit(0);
+	}
+
+	// Create Software Renderer and texture
+	ren = SDL_CreateRenderer(window, -1, SDL_RENDERER_SOFTWARE);
+	tex = SDL_CreateTexture(ren, SDL_PIXELFORMAT_ARGB8888, SDL_TEXTUREACCESS_STREAMING, scr_width, scr_height);
+
+	// Initialize and set PGL context
+	if (!init_glContext(&the_Context, &bbufpix, scr_width, scr_height, 32, 0x00FF0000, 0x0000FF00, 0x000000FF, 0xFF000000)) {
+		puts("Failed to initialize glContext");
+		exit(0);
+	}
+	set_glContext(&the_Context);
+}
+
+void basic_lighting_vs(float* vs_output, void* vertex_attribs, Shader_Builtins* builtins, void* uniforms)
+{
+	My_Uniforms* u = (My_Uniforms*)uniforms;
+
+	mat4 model = u->model;
+	mat4 view = u->view;
+	mat4 projection = u->projection;
+
+	// The 1 in w is there by default according to spec
+	vec4 aPos = ((vec4*)vertex_attribs)[0];
+	vec3 aNormal = vec3(((vec4*)vertex_attribs)[1]);
+
+	*(vec4*)&builtins->gl_Position = projection * view * model * aPos;
+
+	// gouraud shading
+	// ----------------------
+	vec3 Position = vec3(model * aPos);
+	vec3 Normal = mat3(transpose(inverse(model))) * aNormal;
+
+	// ambient
+	float ambientStrength = 0.1f;
+	vec3 ambient = ambientStrength * u->lightColor;
+
+	// diffuse
+	vec3 norm = normalize(Normal);
+	vec3 lightDir = normalize(u->lightPos - Position);
+	float diff = max(dot(norm, lightDir), 0.0f);
+	vec3 diffuse = diff * u->lightColor;
+
+	// specular
+	float specularStrength = 1.0f; // this is set higher to better show the effect of Gouraud shading
+	vec3 viewDir = normalize(u->viewPos - Position);
+	vec3 reflectDir = reflect(-lightDir, norm);
+	float spec = pow(max(dot(viewDir, reflectDir), 0.0f), 32);
+	vec3 specular = specularStrength * spec * u->lightColor;
+
+	// out vec3 LightingColor
+	*(vec3*)vs_output = ambient + diffuse + specular;
+}
+
+void basic_lighting_fs(float* fs_input, Shader_Builtins* builtins, void* uniforms)
+{
+	My_Uniforms* u = (My_Uniforms*)uniforms;
+
+	vec3 LightingColor = ((vec3*)fs_input)[0];
+
+	*(vec4*)&builtins->gl_FragColor = vec4(LightingColor * u->objectColor, 1.0f);
+}
+
+void light_cube_vs(float* vs_output, void* vertex_attribs, Shader_Builtins* builtins, void* uniforms)
+{
+	My_Uniforms* u = (My_Uniforms*)uniforms;
+
+	mat4 model = u->model;
+	mat4 view = u->view;
+	mat4 projection = u->projection;
+
+	// The 1 in w is there by default according to spec
+	vec4 aPos = ((vec4*)vertex_attribs)[0];
+
+	*(vec4*)&builtins->gl_Position = projection * view * model * aPos;
+}
+
+void light_cube_fs(float* fs_input, Shader_Builtins* builtins, void* uniforms)
+{
+	//builtins->gl_FragColor = make_vec4(1.0f, 1.0f, 1.0f, 1.0f);
+	*(vec4*)&builtins->gl_FragColor = vec4(1.0f);
+}
+
+void cleanup()
+{
+	free_glContext(&the_Context);
+
+	SDL_DestroyTexture(tex);
+	SDL_DestroyRenderer(ren);
+	SDL_DestroyWindow(window);
+
+	SDL_Quit();
+}
+
+
+
+
+
+
--- src/2.lighting/premake5.lua
+++ src/2.lighting/premake5.lua
@@ -10,3 +10,15 @@ project "2.2.1.basic_lighting_diffuse"
 project "2.2.2.basic_lighting_specular"
 	kind "ConsoleApp"
 	files "basic_lighting_specular.cpp"
+
+project "2.2.3.basic_lighting_exercise1"
+	kind "ConsoleApp"
+	files "basic_lighting_exercise1.cpp"
+
+project "2.2.4.basic_lighting_exercise2"
+	kind "ConsoleApp"
+	files "basic_lighting_exercise2.cpp"
+
+project "2.2.5.basic_lighting_exercise3"
+	kind "ConsoleApp"
+	files "basic_lighting_exercise3.cpp"
